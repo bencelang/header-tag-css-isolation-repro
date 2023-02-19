12eb0ba3e9aa12a844adc477c7fece04069860e9
Bence Lang <bence.lang@avolantis.net>
2023-02-19 20:35:05 +0100

    Initialized repoduction repository

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace header_tag_css_isolation_repro.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
